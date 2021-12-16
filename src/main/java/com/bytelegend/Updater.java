9bdc97ccf59c1e2b90120ddd850fad5a9b0a6191
wjl-lab <68649094+wjl-lab@users.noreply.github.com>
2021-12-16 10:58:52 +0000

    Update at 2021-12-16T10:58:52.995316Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,10 +2,10 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, new Cat(newCatName));
+        updateToNewCat(home.cat, newCatName);
     }
 
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat.name = newCat.name;
+    private static void updateToNewCat(Cat cat, String newCatName) {
+        cat.name = newCatName;
     }
 }
