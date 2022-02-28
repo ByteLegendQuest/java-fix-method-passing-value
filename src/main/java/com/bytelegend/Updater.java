101e43b9ad2dd30b7406e9d649c2e17dbbb91b52
o0hana <2487311872@qq.com>
2022-02-28 10:04:36 +0000

    Update at 2022-02-28T10:04:36.828432Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,10 +2,11 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, new Cat(newCatName));
+        home.cat=updateToNewCat(home.cat, new Cat(newCatName));
     }
 
-    private static void updateToNewCat(Cat cat, Cat newCat) {
+    public static Cat updateToNewCat(Cat cat, Cat newCat) {
         cat = newCat;
+        return cat;
     }
 }
