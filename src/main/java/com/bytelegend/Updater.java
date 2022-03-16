1f0b411368a6a43e742469291b515878f92ef815
lemon-altxx <2515782282@qq.com>
2022-03-16 14:14:55 +0000

    Update at 2022-03-16T14:14:55.738426Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -3,10 +3,10 @@ package com.bytelegend;
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
         Cat newCat = new Cat(newCatName);
-        updateToNewCat(home.cat, newCat);
+        updateToNewCat(home, newCat);
     }
 
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat = newCat;
+    private static void updateToNewCat(Home home, Cat newCat) {
+        home.cat = newCat;
     }
 }
