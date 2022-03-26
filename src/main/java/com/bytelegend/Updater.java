530a39b0dd4b2be8237cac592b87d7d352db410e
yunghf <yung.hf@outlook.com>
2022-03-26 02:06:24 +0000

    Update at 2022-03-26T02:06:24.749164Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -5,7 +5,7 @@ public class Updater {
         updateToNewCat(home.cat, new Cat(newCatName));
     }
 
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat = newCat;
+    private static void updateToNewCat(Home home, Cat newCat) {
+        home.cat = newCat;
     }
 }
