2fa2cfa72bfb84a4e1bbcde5b99d0e93695e17e2
hesitatemore <1299136605@qq.com>
2022-04-09 09:24:04 +0000

    Update at 2022-04-09T09:24:04.010988Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -3,6 +3,6 @@ package com.bytelegend;
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
 
-        home.cat.name = newCatName;
+        home.cat =  new Cat(newCatName);
     }
 }
