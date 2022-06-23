a8c57b5477467d8479a45f83d90e743047544840
batman405 <2638603343@qq.com>
2022-06-23 03:29:57 +0000

    Update at 2022-06-23T03:29:57.035985Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,10 +2,10 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, newCatName);
+        updateToNewCat(home, new Cat(newCatName));
     }
 
-    private static void updateToNewCat(Cat cat, String newCat) {
-        cat.name = newCat;
+    private static void updateToNewCat(Home home, Cat newCat) {
+        home.cat = newCat;
     }
 }
