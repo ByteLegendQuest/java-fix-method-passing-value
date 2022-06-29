72504aa9c036b68411912331c8ebc846ddd76c1c
2809769075 <2809769075@qq.com>
2022-06-29 10:46:50 +0000

    Update at 2022-06-29T10:46:50.103300Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,10 +2,16 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, new Cat(newCatName));
+       Home home2 = updateToNewCat(home.cat, new Cat(newCatName));
+       home.cat = home2.cat;
+
     }
 
-    private static void updateToNewCat(Cat cat, Cat newCat) {
+    private static Home updateToNewCat(Cat cat, Cat newCat) {
+       
         cat = newCat;
+         Home h = new home();
+        h.cat = cat;
+        return h;
     }
 }
