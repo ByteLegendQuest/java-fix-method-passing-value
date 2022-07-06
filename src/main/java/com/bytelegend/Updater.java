8ee611ed9e7909b450f0146429fa78cb92825f4a
JudgementH <446995859@qq.com>
2022-07-06 16:58:17 +0000

    Update at 2022-07-06T16:58:17.333229Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,11 +2,6 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        // updateToNewCat(home.cat, new Cat(newCatName));
         home.cat = new Cat(newCatName);
-    }
-
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat.name = newCat.name;
     }
 }
