5f6e9314b087e2d51e7410e679da33d1bca9324b
tiger0961 <29515547@qq.com>
2022-10-12 03:10:56 +0000

    Update at 2022-10-12T03:10:56.614664Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -4,8 +4,8 @@ public class Updater {
     public static void updateCat(Home home, String newCatName) {
         updateToNewCat(home.cat, new Cat(newCatName));
     }
-
+ 
     private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat = newCat;
+        cat.name = newCat.name;
     }
 }
