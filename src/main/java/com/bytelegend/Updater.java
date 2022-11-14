28f210107ad96d2b0fd663f52b3c5891b2973400
pamaomaoa <544060246@qq.com>
2022-11-14 16:05:34 +0000

    Update at 2022-11-14T16:05:34.279933Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -1,11 +1,11 @@
 package com.bytelegend;
 
 public class Updater {
-    public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, new Cat(newCatName));
+  public static void updateCat(Home home, String newCatName) {
+        updateToNewCat(home, new Cat(newCatName));
     }
 
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat = newCat;
+    private static void updateToNewCat(Home home, Cat newCat) {
+        home.cat = newCat;
     }
 }
