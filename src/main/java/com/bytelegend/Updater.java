c6d8cf63673791d4a46c79a093b7f3acabd77f80
Newelle <wozi1988@proton.me>
2022-11-14 12:05:47 +0000

    Update at 2022-11-14T12:05:47.433291Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -1,9 +1,10 @@
-public class Updater {
+
     public static void updateCat(Home home, String newCatName) {
-        home.cat = new Cat(newCatName);
+        updateToNewCat(home.cat, new Cat(newCatName));
     }
 
+ 
     private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat = newCat;
+        cat.name = newCat.name;
     }
 }
