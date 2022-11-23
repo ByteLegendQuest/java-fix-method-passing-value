e83ba9ebd5549831307ad58cd2b71b93c49cd5c3
williamgl <ligan@oregonstate.edu>
2022-11-23 00:45:13 +0000

    Update at 2022-11-23T00:45:13.976341Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,6 +2,6 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        home.cat = new Cat(newCatName)
+        home.cat = new Cat(newCatName);
     }
 }
