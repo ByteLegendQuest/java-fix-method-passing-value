fd33b5c34ea0452316a7a522cc2988bc1878718a
TheTenthYama <1355794322@qq.com>
2023-01-12 06:53:14 +0000

    Update at 2023-01-12T06:53:14.142398Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,6 +2,7 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
+        home.cat = new Cat(newCatName);
         updateToNewCat(home.cat, new Cat(newCatName));
     }
 
