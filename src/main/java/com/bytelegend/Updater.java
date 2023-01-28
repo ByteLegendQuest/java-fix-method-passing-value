c1d0d03504387e77833455a52ba32c7ef7178405
andyliukun <744059787@qq.com>
2023-01-28 08:49:47 +0000

    Update at 2023-01-28T08:49:47.265937Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,7 +2,7 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, new Cat(newCatName));
+        home.cat = new Cat(newCatName);
     }
 
     private static void updateToNewCat(Cat cat, Cat newCat) {
