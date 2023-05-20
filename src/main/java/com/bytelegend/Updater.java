8e8d79c5ec679732e5a0b71d6b7fc9d2156fc582
danthe1st <daniel@wwwmaster.at>
2023-05-20 15:35:00 +0000

    Update at 2023-05-20T15:35:00.465323Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -3,10 +3,6 @@ package com.bytelegend;
 public class Updater {
 
     public static void updateCat(Home home, String newCatName) {
-        home.cat, new Cat(newCatName);
-    }
-
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat.name = newCat.name;
+        home.cat = new Cat(newCatName);
     }
 }
