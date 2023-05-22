fa2cf93e05f00a6c061e9ca7ef88b4714127234d
NahuelBullonJensen <nahuelbullonjensen@gmail.com>
2023-05-22 02:08:17 +0000

    Update at 2023-05-22T02:08:17.506195Z

--- src/main/java/com/bytelegend/Updater.java
+++ src/main/java/com/bytelegend/Updater.java
@@ -2,10 +2,6 @@ package com.bytelegend;
 
 public class Updater {
     public static void updateCat(Home home, String newCatName) {
-        updateToNewCat(home.cat, new Cat(newCatName));
-    }
-
-    private static void updateToNewCat(Cat cat, Cat newCat) {
-        cat.name = newCat.name;
+        home.cat = new Cat(newCatName);
     }
 }
